da6cb1030fcd970085ead03c9de9a8ad228722ae
gatienc <gatien_dev@chenu.me>
2024-06-03 23:48:46 +0200

    wip

---
+++ scripts/post_generation.py
@@ -0,0 +1,21 @@
+import subprocess
+from collections import OrderedDict
+
+
+def git_init():
+    subprocess.call(['git', 'init'])
+    subprocess.call(['git', 'add', '*'])
+    subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+
+
+def github_init():
+    subprocess.call(['gh', 'repo', 'create',
+                    cookiecutter["repo_name"], '--private', '--source=.', '--remote=upstream'])
+
+
+if __name__ == '__main__':
+    git_init()
+
+    if cookiecutter["use_github"] == 'y':
+        github_init()
+    subprocess.call(['make', 'create_environment'])
